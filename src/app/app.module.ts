9116303d558129685f23ae1f16229fcd0ea785c7
simzim <info@simzim.lt>
2022-04-19 17:05:17 +0300

    istrintas nereikalingas componentas, pataisytas active menu, suvienodintas db rodymas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { MatInputModule } from '@angular/material/input';
 import { UnitFormComponent } from './components/unit-form/unit-form.component';
 import { MatSelectModule } from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
-import { PrefixComponent } from './components/prefix/prefix.component';
+// import { PrefixComponent } from './components/prefix/prefix.component';
 import { PrefixesComponent } from './pages/prefixes/prefixes.component';
 
 @NgModule({
@@ -26,7 +26,7 @@ import { PrefixesComponent } from './pages/prefixes/prefixes.component';
     UnitsSIComponent,
     MeniuComponent,
     UnitFormComponent,
-    PrefixComponent,
+
     PrefixesComponent,
   ],
   imports: [
